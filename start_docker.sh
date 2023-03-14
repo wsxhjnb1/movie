8937c0408ed34c44e72bb29b10e89a749d70cc52
yuhao xie <94712496+wsxhjnb1@users.noreply.github.com>
2023-03-13 22:19:26 -0400

    finished the reverse index search (#4)

--- start_docker.sh
+++ start_docker.sh
@@ -1,3 +1,4 @@
 #!/bin/zsh
-docker run -p 8090:8090 -v "`pwd`":/autocomplete --rm -dit go_image:0.2 #go run go_start.go
-
+docker build -f ./autocomplete/Dockerfile -t go_image:0.2 .
+docker run -p 8090:8090 -v "`pwd`/autocomplete":/autocomplete --rm -dit go_image:0.2 go run go_start.go
+docker run --name es01 -v "`pwd`/elasticsearch":/usr/share/elasticsearch/data --rm -p 9200:9200 -p 9300:9300 -dit -e "discovery.type=single-node" docker.elastic.co/elasticsearch/elasticsearch:7.9.3
